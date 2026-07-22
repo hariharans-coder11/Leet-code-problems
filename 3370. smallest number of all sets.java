bf2ec79113907f0e42d0ca4f8c316b9200a906a5
HARIHARAN <hariharan.s@dhaanishcollege.in>
2026-07-22 14:16:20 +0530

    Implement smallestNumber method in Solution class

---
+++ 3370. smallest number of all sets.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int smallestNumber(int n) {
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;
+        n |= n >> 16;
+        return n;
+    }
+}
